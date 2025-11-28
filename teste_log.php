ffb87c77f34bfc6dcb8303e4bf96d52f14b648d8
felippefardin <felippefardin@gmail.com>
2025-11-28 18:05:13 -0300

    Seguranca: Removendo arquivos de debug, logs e vendor do versionamento

--- teste_log.php
+++
@@ -1,9 +0,0 @@
-<?php
-$logDir = __DIR__ . '/logs';
-if (!is_dir($logDir)) {
-    mkdir($logDir, 0777, true);
-}
-$logFile = $logDir . '/register_debug.log';
-file_put_contents($logFile, "[" . date('Y-m-d H:i:s') . "] Teste manual OK\n", FILE_APPEND);
-echo "Arquivo de log criado? Verifique em /logs/register_debug.log";
-?>
--- ver_log.php
+++
@@ -1,8 +0,0 @@
-<?php
-$logPath = __DIR__ . '/../logs/register_debug.log';
-if (file_exists($logPath)) {
-    echo "<pre>" . htmlspecialchars(file_get_contents($logPath)) . "</pre>";
-} else {
-    echo "Log não encontrado.";
-}
-?>
